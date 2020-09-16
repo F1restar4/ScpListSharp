ecbf115d9fa31cbeb5075b1bca98c460f1c63b01
F1restar4 <yeahibiketowork@hitler.rocks>
2020-09-16 13:19:57 -0500

    Initial commit

---
+++ Entities.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace ScpListSharp.Entities
+{
+	public struct SCPServer
+	{
+		/// <summary>
+		/// The server's ID
+		/// </summary>
+		[JsonProperty]
+		public int ID { get; internal set; }
+
+		/// <summary>
+		/// The server's port
+		/// </summary>
+		[JsonProperty]
+		public int Port { get; internal set; }
+
+		/// <summary>
+		/// Last time the server was online. 
+		/// This will only have a date
+		/// </summary>
+		[JsonProperty]
+		public DateTime LastOnline { get; internal set; }
+
+		/// <summary>
+		/// The number of players connected to the server.
+		/// Formatted as x/y
+		/// </summary>
+		[JsonProperty]
+		public string Players { get; internal set; }
+
+		/// <summary>
+		/// List of players connected to the server.
+		/// Null if no players are connected.
+		/// </summary>
+		[JsonProperty]
+		public List<SCPPlayer> PlayerList { get; internal set; }
+
+		[JsonProperty]
+		public string Info { get; internal set; }
+
+		/// <summary>
+		/// The version of the game the server is running.
+		/// </summary>
+		[JsonProperty]
+		public string Version { get; internal set; }
+
+		/// <summary>
+		/// The id of the server's info pastebin.
+		/// </summary>
+		[JsonProperty]
+		public string Pastebin { get; internal set; }
+
+		/// <summary>
+		/// Friendly fire enabled
+		/// </summary>
+		[JsonProperty]
+		public bool FF { get; internal set; }
+
+		/// <summary>
+		/// Whitelist enabled
+		/// </summary>
+		[JsonProperty]
+		public bool WL { get; internal set; }
+
+		/// <summary>
+		/// Server modded
+		/// </summary>
+		[JsonProperty]
+		public bool Modded { get; internal set; }
+
+		[JsonProperty]
+		public int Mods { get; internal set; }
+
+		[JsonProperty]
+		public bool Suppress { get; internal set; }
+
+		internal SCPServer(int ID, int Port, DateTime LastOnline, string Players, List<SCPPlayer> PlayerList, string Info, string Version, string Pastebin, bool FF, bool WL, bool Modded, int Mods, bool Suppress)
+		{
+			this.ID = ID;
+			this.Port = Port;
+			this.LastOnline = LastOnline;
+			this.Players = Players;
+			this.PlayerList = PlayerList;
+			this.Info = Info;
+			this.Version = Version;
+			this.Pastebin = Pastebin;
+			this.FF = FF;
+			this.WL = WL;
+			this.Modded = Modded;
+			this.Mods = Mods;
+			this.Suppress = Suppress;
+		}
+	}
+
+	public struct SCPPlayer
+	{
+		[JsonProperty]
+		public string ID { get; internal set; }
+
+		[JsonProperty]
+		public string Name { get; internal set; }
+		internal SCPPlayer(string ID, string Name)
+		{
+			this.ID = ID;
+			this.Name = Name;
+		}
+	}
+
+	internal struct SCPServersResponse
+	{
+		[JsonProperty]
+		public bool Success { get; internal set; }
+
+		[JsonProperty]
+		public int Cooldown { get; internal set;  }
+
+		[JsonProperty]
+		public List<SCPServer> Servers { get; internal set; }
+
+		internal SCPServersResponse(bool Success, int Cooldown, List<SCPServer> Servers)
+		{
+			this.Success = Success;
+			this.Cooldown = Cooldown;
+			this.Servers = Servers;
+		}
+	}
+}
---
+++ Rest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net.Http;
+using ScpListSharp.Entities;
+using System.Net;
+using System.IO;
+
+namespace ScpListSharp
+{
+	public static class Rest
+	{
+		/// <summary>
+		/// Gets a list of servers owned by the provided account
+		/// </summary>
+		/// <param name="id">Account ID of the server. Use "!id" in the server console to obtain the id. </param>
+		/// <param name="key">API key for the account id. Use "!api" in the server console to obtain the key.</param>
+		/// <returns></returns>
+		public static List<SCPServer> GetOwnServers(int id, string key )
+		{
+			string url = $"https://api.scpslgame.com/serverinfo.php?id={id}&key={key}&lo=true&players=true&list=true&info=true&pastebin=true&version=true&flags=true&nicknames=true";
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.ContentType = "application/json";
+			string data;
+			WebResponse response = request.GetResponse();
+			using (Stream responseStream = response.GetResponseStream() ?? Stream.Null)
+			using (StreamReader responseReader = new StreamReader(responseStream))
+			{
+					data = responseReader.ReadToEnd();
+			}
+			var outResponse = JsonConvert.DeserializeObject<SCPServersResponse>(data);
+			return outResponse.Servers;
+		}
+	}
+}
