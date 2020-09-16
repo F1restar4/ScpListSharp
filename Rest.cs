6b8481e0c0977df78e5bacf1794e9b85f445f4b8
F1restar4 <yeahibiketowork@hitler.rocks>
2020-09-16 13:39:54 -0500

    Implement ratelimits Also async method I should have done this in a separate commit oh well

--- Rest.cs
+++ Rest.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Newtonsoft.Json;
 using System.Web;
 using System.Net.Http;
+using System.Threading.Tasks;
 using ScpListSharp.Entities;
 using System.Net;
 using System.IO;
@@ -12,14 +13,18 @@ namespace ScpListSharp
 {
 	public static class Rest
 	{
+		static DateTime NextAllowed = DateTime.Now;
 		/// <summary>
 		/// Gets a list of servers owned by the provided account
 		/// </summary>
 		/// <param name="id">Account ID of the server. Use "!id" in the server console to obtain the id. </param>
 		/// <param name="key">API key for the account id. Use "!api" in the server console to obtain the key.</param>
 		/// <returns></returns>
-		public static List<SCPServer> GetOwnServers(int id, string key )
+		public static async Task<List<SCPServer>> GetOwnServersAsync(int id, string key)
 		{
+			if (NextAllowed > DateTime.Now)
+				throw new WebException($"You are being rate limited, try again at: {NextAllowed}");
+
 			string url = $"https://api.scpslgame.com/serverinfo.php?id={id}&key={key}&lo=true&players=true&list=true&info=true&pastebin=true&version=true&flags=true&nicknames=true";
 			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 			request.ContentType = "application/json";
@@ -28,9 +33,30 @@ namespace ScpListSharp
 			using (Stream responseStream = response.GetResponseStream() ?? Stream.Null)
 			using (StreamReader responseReader = new StreamReader(responseStream))
 			{
-					data = responseReader.ReadToEnd();
+					data = await responseReader.ReadToEndAsync();
 			}
 			var outResponse = JsonConvert.DeserializeObject<SCPServersResponse>(data);
+			NextAllowed = DateTime.Now.AddSeconds(outResponse.Cooldown);
+			return outResponse.Servers;
+		}
+
+		public static List<SCPServer> GetOwnServers(int id, string key)
+		{
+			if (NextAllowed > DateTime.Now)
+				throw new WebException($"You are being rate limited, try again at: {NextAllowed}");
+
+			string url = $"https://api.scpslgame.com/serverinfo.php?id={id}&key={key}&lo=true&players=true&list=true&info=true&pastebin=true&version=true&flags=true&nicknames=true";
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.ContentType = "application/json";
+			string data;
+			WebResponse response = request.GetResponse();
+			using (Stream responseStream = response.GetResponseStream() ?? Stream.Null)
+			using (StreamReader responseReader = new StreamReader(responseStream))
+			{
+				data = responseReader.ReadToEnd();
+			}
+			var outResponse = JsonConvert.DeserializeObject<SCPServersResponse>(data);
+			NextAllowed = DateTime.Now.AddSeconds(outResponse.Cooldown);
 			return outResponse.Servers;
 		}
 	}
